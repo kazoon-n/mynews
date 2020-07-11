55840a66ee2f1099521551bcd527a1dbf1d43301
EC2 Default User <ec2-user@ip-172-31-30-82.us-east-2.compute.internal>
2020-07-11 07:36:39 +0000

    laravel12

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,14 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     // practice 09
-    Route::get('profile/create', 'Admin\ProfileController@add'); 
-    Route::get('profile/edit', 'Admin\ProfileController@edit'); 
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth'); 
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth'); 
 });
 
 // practice 09
 Route::get('XXX', 'Admin\AAAController@bbb');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
