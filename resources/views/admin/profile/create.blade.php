724eab6a545fe8cad6fcfd28eaf02178207fa0b3
EC2 Default User <ec2-user@ip-172-31-30-82.us-east-2.compute.internal>
2020-07-11 08:22:44 +0000

    laravel 13

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -7,6 +7,51 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h1>Myプロフィール作成画面</h1>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                @if(count($errors) > 0)
+                    <ul>
+                        @foreach($errors->all() as $e)
+                            <li>{{ $e }}</li>
+                        @endforeach
+                    </ul>
+                @endif
+                
+                <div class="form-group row">
+                    <label class="col-md-2">氏名</label>
+                    <div class="col-md-10">
+                        <input type="text" class="form-control" name="name" value="{{ old('name') }}" />
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-md-2">性別</label>
+                    <div class="col-md-3">
+                        <select name="gender" class="form-control">
+                            <option value="male">男性</option>
+                            <option value="female">女性</option>
+                            <option value="other">その他</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-md-2">趣味</label>
+                    <div class="col-md-10">
+                        <input type="text" class="form-control" name="hobby" value="{{ old('name') }}" />
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-md-2">自己紹介欄</label>
+                    <div class="col-md-10">
+                        <textarea class="form-control" name="introduction" cols="83" rows="15">{{ old('introduction') }}</textarea>
+                    </div>
+                </div>
+                
+                {{ csrf_field() }}
+                <input type="submit" class="btn btn-primary" value="更新">
+                    
+                </form>
             </div>
         </div>
     </div>
