73eb064b662e22fbcf9fef7a1893802afbf030a5
EC2 Default User <ec2-user@ip-172-31-30-82.us-east-2.compute.internal>
2020-08-10 16:52:18 +0000

    Laravel 19

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Profile;
+
+class ProfileController extends Controller
+{
+    //
+    public function index(Request $request){
+        $profiles = Profile::all()->sortByDesc('updated_at');
+        
+        if(count($profiles) > 0){
+            $headline = $profiles->shift();
+        }else{
+            $headline = null;
+        }
+        
+        return view('profile.index', [
+                'headline' => $headline,
+                'profiles' => $profiles,
+            ]);
+    }
+}
--- app/Profile.php
+++ app/Profile.php
@@ -21,3 +21,4 @@ class Profile extends Model
     
     
 }
+ 
